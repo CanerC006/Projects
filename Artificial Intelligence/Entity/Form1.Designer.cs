dd9f36caeea6e0184e1a9c065ed5e426d7d53dc2
CanerC006 <63341024+CanerC006@users.noreply.github.com>
2020-07-01 10:38:07 +0300

    Add files via upload
    the program works as follows : you can save information on database by artificial intelligence and your microphone.
    Firstly you need write something in the text after that click first button after you should say '' hello '' or '' hi '' after information will save on your database

---
+++ Artificial Intelligence/Entity/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace CanerCandan_21897853
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKonus = new System.Windows.Forms.Button();
+            this.txtKonus = new System.Windows.Forms.TextBox();
+            this.txtOgrNo = new System.Windows.Forms.TextBox();
+            this.txtSoyadi = new System.Windows.Forms.TextBox();
+            this.txtAdi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnKonus
+            // 
+            this.btnKonus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKonus.Location = new System.Drawing.Point(12, 12);
+            this.btnKonus.Name = "btnKonus";
+            this.btnKonus.Size = new System.Drawing.Size(173, 63);
+            this.btnKonus.TabIndex = 0;
+            this.btnKonus.Text = "Konuşun";
+            this.btnKonus.UseVisualStyleBackColor = true;
+            this.btnKonus.Click += new System.EventHandler(this.btnKonus_Click);
+            // 
+            // txtKonus
+            // 
+            this.txtKonus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKonus.Location = new System.Drawing.Point(214, 30);
+            this.txtKonus.Multiline = true;
+            this.txtKonus.Name = "txtKonus";
+            this.txtKonus.Size = new System.Drawing.Size(320, 27);
+            this.txtKonus.TabIndex = 1;
+            this.txtKonus.TextChanged += new System.EventHandler(this.BindingContext_Changed);
+            // 
+            // txtOgrNo
+            // 
+            this.txtOgrNo.Location = new System.Drawing.Point(448, 134);
+            this.txtOgrNo.Multiline = true;
+            this.txtOgrNo.Name = "txtOgrNo";
+            this.txtOgrNo.Size = new System.Drawing.Size(79, 27);
+            this.txtOgrNo.TabIndex = 1;
+            // 
+            // txtSoyadi
+            // 
+            this.txtSoyadi.Location = new System.Drawing.Point(262, 134);
+            this.txtSoyadi.Multiline = true;
+            this.txtSoyadi.Name = "txtSoyadi";
+            this.txtSoyadi.Size = new System.Drawing.Size(79, 27);
+            this.txtSoyadi.TabIndex = 1;
+            // 
+            // txtAdi
+            // 
+            this.txtAdi.Location = new System.Drawing.Point(65, 134);
+            this.txtAdi.Multiline = true;
+            this.txtAdi.Name = "txtAdi";
+            this.txtAdi.Size = new System.Drawing.Size(79, 27);
+            this.txtAdi.TabIndex = 1;
+            this.txtAdi.TextChanged += new System.EventHandler(this.BindingContext_Changed);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(9, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Adı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(187, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Soyadı : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(374, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ögr No :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAdi);
+            this.Controls.Add(this.txtSoyadi);
+            this.Controls.Add(this.txtOgrNo);
+            this.Controls.Add(this.txtKonus);
+            this.Controls.Add(this.btnKonus);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKonus;
+        private System.Windows.Forms.TextBox txtKonus;
+        private System.Windows.Forms.TextBox txtOgrNo;
+        private System.Windows.Forms.TextBox txtSoyadi;
+        private System.Windows.Forms.TextBox txtAdi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Artificial Intelligence/Entity/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Net;
+using System.Net.Sockets;
+using System.Globalization;
+
+namespace CanerCandan_21897853             // HOCAM EĞER İSİM ŞİFRE İSTERSE  :  Acc : MasterFF / Pass : 4565123644aa  // EntityFramework ' ü Sql isim şifresine göre ayarladım. 
+{
+    public partial class Form1 : Form
+    {
+        SpeechEntities db;
+        
+        SpeechRecognitionEngine recoEngine = new SpeechRecognitionEngine();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnKonus_Click(object sender, EventArgs e)
+        {
+            btnKonus.Text = "Dinliyorum...";
+            sestanima_ayarlari();
+            recoEngine.RecognizeAsync();
+        }
+        public void sestanima_ayarlari()
+        {
+            string[] ihtimaller = { "hi", "hello" };
+            Choices secenekler = new Choices(ihtimaller);
+            Grammar grammer = new Grammar(new GrammarBuilder(secenekler));
+            recoEngine.LoadGrammar(grammer);
+            recoEngine.SetInputToDefaultAudioDevice();
+            recoEngine.SpeechRecognized += ses_tanıdığında;
+        }
+        private void ses_tanıdığında(object sender, SpeechRecognizedEventArgs e)
+        {
+            if(e.Result.Text=="hi")
+            {
+                txtKonus.Text = "hi";
+            }
+            else if (e.Result.Text == "hello")
+            {
+                txtKonus.Text = "hello";
+
+            }
+   
+        }
+        private void AddEventHandler()
+        {
+            txtKonus.BindingContextChanged += new EventHandler(BindingContext_Changed);
+        }
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+        private void BindingContext_Changed(object sender, EventArgs e)
+        {
+            if(txtKonus.Text == "hello")
+            {
+                Save_data();
+            }
+        }
+        public void Save_data()
+        {
+
+            try
+            {
+                db = new SpeechEntities();
+                AddEventHandler();
+                tbl_bilgiler info = new CanerCandan_21897853.tbl_bilgiler();
+                info.Adi = txtAdi.Text;
+                info.Soyadi = txtSoyadi.Text;
+                info.Ogr_No = txtOgrNo.Text;
+                DateTime localDate = DateTime.Now;
+                info.Zaman = localDate;
+                string LocalIpAdress = GetLocalIPAddress();
+                info.IP = LocalIpAdress;
+                MessageBox.Show("Adi : " + info.Adi + "Soyadi : " + info.Soyadi + "Ogr No : " + info.Ogr_No + "Zaman : " + info.Zaman + "İP : " + info.IP);
+                db.tbl_bilgiler.Add(info);
+                db.SaveChanges();
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+
+
+        }
+
+    }
+}
